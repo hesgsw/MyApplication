ccada912624a867286295780ca2d2a9f2458d2f0
hesgsw <603085061@qq.com>
2020-03-22 16:38:34 +0800

    test4

---
+++ app/src/main/java/com/example/he_sgsw/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.he_sgsw.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.tv_biang).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"sfdfefdoi",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
